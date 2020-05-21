356308f62545d7a26db5464bb6082f88cc202838
stLmpp <gui.stlmpp@hotmail.com>
2020-05-21 20:12:29 -0300

    - fixed missing export (query)

--- projects/st-store/src/public-api.ts
+++ projects/st-store/src/public-api.ts
@@ -6,4 +6,5 @@ export * from './lib/st-store.module';
 export * from './lib/type';
 export * from './lib/map';
 export * from './lib/st-store';
+export * from './lib/st-query';
 export { enableProd } from './lib/env';
