112faf73298a876a222a4f9778d80dbc6776a556
stLmpp <gui.stlmpp@hotmail.com>
2020-05-21 22:42:35 -0300

    - fixed missing exports operators

--- projects/st-store/src/public-api.ts
+++ projects/st-store/src/public-api.ts
@@ -7,4 +7,5 @@ export * from './lib/type';
 export * from './lib/map';
 export * from './lib/st-store';
 export * from './lib/st-query';
+export * from './lib/operators';
 export { enableProd } from './lib/env';
