7260e1eeb23c7000798def7e355890bb3f45a926
stLmpp <gui.stlmpp@hotmail.com>
2020-09-26 11:03:04 -0300

    - fix the typing error in the sumBy function

--- projects/stlmpp/utils/src/lib/sum.ts
+++ projects/stlmpp/utils/src/lib/sum.ts
@@ -11,13 +11,12 @@ export function sum(values: number[]): number {
 export function sumBy<T = any>(values: T[], key: keyof T | (keyof T)[] | string | string[]): number {
   const keyIsArray = isArray(key);
   if (!values?.length || !key || (keyIsArray && !(key as any[])?.length)) return 0;
-  const get = keyIsArray || (isString(key) && key.includes('.')) ? getDeep : value => value[key];
+  const get = keyIsArray || (isString(key) && key.includes('.')) ? getDeep : (value: T) => (value as any)[key];
   return values.reduce((acc, item) => acc + +(get(item, key as string) ?? 0), 0);
 }
 
 export const sumOperator = () => map<number[], number>(sum);
-export const sumByOperator = <T>(key: keyof T | (keyof T)[]) =>
-  map<T[], number>(values => sumBy(values, key));
+export const sumByOperator = <T>(key: keyof T | (keyof T)[]) => map<T[], number>(values => sumBy(values, key));
 
 @Pipe({ name: 'stSumBy' })
 export class SumByPipe implements PipeTransform {
