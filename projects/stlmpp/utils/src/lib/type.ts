3589e8fda8854f0c8a79576093cefa3a0ba7ffdb
stLmpp <gui.stlmpp@hotmail.com>
2020-11-24 17:04:38 -0300

    feat(utils): added coerceBooleanProperty function and BooleanInput type

--- projects/stlmpp/utils/src/lib/type.ts
+++ projects/stlmpp/utils/src/lib/type.ts
@@ -1,3 +1,4 @@
 export type ID = string | number;
 export type IdGetter<T, S = number> = (entity: T) => S;
 export type IdGetterType<T, S extends ID = number> = IdGetter<T, S> | string | string[] | keyof T;
+export type BooleanInput = string | boolean | null | undefined;
