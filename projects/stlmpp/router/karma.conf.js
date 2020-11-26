6a37c302af48371bc80b81f3ba7ed649e539154b
stLmpp <gui.stlmpp@hotmail.com>
2020-11-26 09:46:16 -0300

    feat(): migrate from karma-coverage-istanbul-reporter to karma-coverage update(): update karma to latest version

--- projects/stlmpp/router/karma.conf.js
+++ projects/stlmpp/router/karma.conf.js
@@ -9,17 +9,20 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-jasmine-seed-reporter'),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
+    coverageReporter: {
       dir: require('path').join(__dirname, '../../../coverage/stlmpp/router'),
-      reports: ['html', 'lcovonly', 'text-summary', 'json'],
-      fixWebpackSourcePaths: true,
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }, { type: 'lcovonly' }, { type: 'json' }],
     },
     reporters: ['progress', 'kjhtml', 'jasmine-seed'],
     port: 9876,
