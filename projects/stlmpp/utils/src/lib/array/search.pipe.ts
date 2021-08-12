cfe177485d72dda7f066e436f92fe4efa8feed31
stLmpp <gui.stlmpp@hotmail.com>
2021-08-12 15:16:08 -0300

    feat(utils): SearchPipe now accepts null and undefined in the array and term argument

--- projects/stlmpp/utils/src/lib/array/search.pipe.ts
+++ projects/stlmpp/utils/src/lib/array/search.pipe.ts
@@ -3,7 +3,14 @@ import { arraySearch } from 'st-utils';
 
 @Pipe({ name: 'search' })
 export class SearchPipe implements PipeTransform {
-  transform<T, K extends keyof T>(array: T[], keyOrKeysOrCallback: K | K[] | ((item: T) => T[K]), term: string): T[] {
+  transform<T, K extends keyof T>(
+    array: T[] | null | undefined,
+    keyOrKeysOrCallback: K | K[] | ((item: T) => T[K]),
+    term: string | null | undefined
+  ): T[] {
+    if (!array) {
+      return [];
+    }
     return arraySearch(array, keyOrKeysOrCallback, term);
   }
 }
