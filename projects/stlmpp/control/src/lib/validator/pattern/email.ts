f2a0fcc627513930318ffe311fc2d25470f92fff
Guilherme Pais <guilherme.pais@accenture.com>
2022-05-30 16:52:04 -0300

    refactor(control): small change to email regex

--- projects/stlmpp/control/src/lib/validator/pattern/email.ts
+++ projects/stlmpp/control/src/lib/validator/pattern/email.ts
@@ -3,7 +3,7 @@ import { Control } from '../../control/control';
 import { Nullable } from '../../util';
 
 const EMAIL_REGEXP =
-  /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z\d!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z\d!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z\d](?:[a-zA-Z\d-]{0,61}[a-zA-Z\d])?(?:\.[a-zA-Z\d](?:[a-zA-Z\d-]{0,61}[a-zA-Z\d])?)*$/;
 
 export abstract class AbstractEmailValidator extends ControlValidator<Nullable<string>, boolean> {
   readonly name: string = 'email';
